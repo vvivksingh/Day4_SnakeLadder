e41832288a1978f03e197f7833b2af354c062af7
Vivek <vvivksingh@gmail.com>
2021-12-21 18:37:25 +0530

    SnakeNLadder Use Case 4

--- SnakeLadderUC4.java
+++ SnakeLadderUC4.java
@@ -1,28 +1,124 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
 
-public class SnakeLadderUC4{
-	public static void main(String[] args){
-		
-		double[][] arr = new double[][]{
-					  {1,2,3,4,5,6,7,8,9,10},
-					  {11,12,13,14,15,16,17,18,19,20},
-					  {21,22,23,24,25,26,27,28,29,30},
-					  {31,32,33,34,35,36,37,38,39,40},
-					  {41,42,43,44,45,46,47,48,49,50},
-					  {51,52,53,54,55,56,57,58,59,60},
-					  {61,62,63,64,65,66,67,68,69,70},
-					  {71,72,73,74,75,76,77,78,79,80},
-  					  {81,82,83,84,85,86,87,88,89,90},
-					  {91,91,93,94,95,96,97,98,99,100}
-					 };
-		//Input
+public class SnakeLadderUC4 {
 
-		for(int i=0; i>10; i++){
-                        //columns
-                        for(int j=0; j>10; j++){
-                                System.out.println(arr[i][j]);
-			}
-		}
+	public static void main(String[] args) {
+		SnakeNLadder s = new SnakeNLadder();
+		s.startGame();
 
 	}
+
+}
+
+class SnakeNLadder{
+
+	final static int WINPOINT = 100;
+
+
+	static Map<Integer,Integer> snake = new HashMap<Integer,Integer>();
+	static Map<Integer,Integer> ladder = new HashMap<Integer,Integer>();
+
+	{
+		snake.put(99,54);
+		snake.put(70,55);
+		snake.put(52,42);
+		snake.put(25,2);
+		snake.put(95,72);
+
+		ladder.put(6,25);
+		ladder.put(11,40);
+		ladder.put(60,85);
+		ladder.put(46,90);
+		ladder.put(17,69);
+	}
+
+
+
+	public int rollDice()
+	{
+		int n = 0;
+		Random r = new Random();
+		n=r.nextInt(7);
+		return (n==0?1:n);
+	}
+
+	public void startGame()
+	{
+		int player1 =0, player2=0;
+		int currentPlayer=-1;
+		Scanner s = new Scanner(System.in);
+		String str;
+		int diceValue =0;
+		do
+		{
+			System.out.println(currentPlayer==-1?"\n\nFIRST PLAYER TURN":"\n\nSECOND PLAYER TURN");
+			System.out.println("Press r to roll Dice");
+			str = s.next();
+			diceValue = rollDice();
+			System.out.println("You get : "+ diceValue);
+
+			if(currentPlayer == -1)
+			{
+				player1 = calculatePlayerValue(player1,diceValue);
+				System.out.println("First Player :: " + player1);
+				System.out.println("Second Player :: " + player2);
+				System.out.println("------------------");
+				if(isWin(player1))
+				{
+					System.out.println("First player wins");
+					return;
+				}
+			}
+			else
+			{
+				player2 = calculatePlayerValue(player2,diceValue);
+				System.out.println("First Player :: " + player1);
+				System.out.println("Second Player :: " + player2);
+				System.out.println("------------------");
+				if(isWin(player2))
+				{
+					System.out.println("Second player wins");
+					return;
+				}
+			}
+
+			currentPlayer= -currentPlayer;
+
+
+		}while("r".equals(str));
+	}
+
+
+	public int calculatePlayerValue(int player, int diceValue)
+	{
+		player = player + diceValue;
+
+		if(player > WINPOINT)
+		{
+			player = player - diceValue;
+			return player;
+		}
+
+		if(null!=snake.get(player))
+		{
+			System.out.println("swallowed by snake");
+			player= snake.get(player);
+		}
+
+		if(null!=ladder.get(player))
+		{
+			System.out.println("climb up the ladder");
+			player= ladder.get(player);
+		}
+		return player;
+	}
+
+	public boolean isWin(int player)
+	{
+		return WINPOINT == player;
+	}
+
 }
